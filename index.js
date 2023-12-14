ce542c44bd9914489228d000f1dd6307e54df6db
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2023-12-14 19:37:59 +0530

    automatic take url from tab, add copy button

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 const frontendBtn = document.getElementById("frontend-btn");
 const backendBtn = document.getElementById("backend-btn");
+const copyBtn = document.getElementById("copy-btn");
 const inputBox = document.getElementById("input-box");
 
-frontendBtn.addEventListener("click", () => {
-  //   console.log("test1", Window.location);
-  //   console.log("test2", chrome.webNavigation);
-  const link = inputBox.value;
-  if (!link) {
-    alert("Please enter the Workspace URL");
-    return;
-  }
+let currentURL;
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  currentURL = tabs[0].url;
 
-  const extractedLink = link.split(".vm")[0];
-
+  const extractedLink = currentURL.split(".vm")[0];
   const replacedLink = extractedLink.replaceAll("-", ".");
 
-  const frontendLink = (replacedLink + ":8081").replace("https://", "http://");
-  window.open(frontendLink, "_blank");
-  //   console.log(frontendLink);
-});
+  frontendBtn.addEventListener("click", () => {
+    if (!currentURL.includes("vm-workspace.crio.do")) {
+      alert("Bhakk! It's not Workspace!");
+      return;
+    }
 
-backendBtn.addEventListener("click", () => {
-  const link = inputBox.value;
+    const frontendLink = (replacedLink + ":8081").replace(
+      "https://",
+      "http://"
+    );
+    window.open(frontendLink, "_blank");
+  });
 
-  if (!link) {
-    alert("Please enter the Workspace URL");
-    return;
-  }
+  backendBtn.addEventListener("click", () => {
+    if (!currentURL.includes("vm-workspace.crio.do")) {
+      alert("Bhakk! It's not Workspace!");
+      return;
+    }
 
-  const extractedLink = link.split(".vm")[0];
+    const backendLink = (replacedLink + ":8082").replace("https://", "http://");
+    window.open(backendLink, "_blank");
+  });
 
-  const replacedLink = extractedLink.replaceAll("-", ".");
+  copyBtn.addEventListener("click", () => {
+    if (!currentURL.includes("vm-workspace.crio.do")) {
+      alert("Bhakk! It's not Workspace!");
+      return;
+    }
 
-  const backendLink = (replacedLink + ":8082").replace("https://", "http://");
-  window.open(backendLink, "_blank");
-  //   console.log(frontendLink);
+    const copiedLink = (replacedLink + ":8082").replace("https://", "http://");
+    navigator.clipboard.writeText(copiedLink);
+    copyBtn.textContent = "Copied!";
+    // window.open(backendLink, "_blank");
+  });
 });
